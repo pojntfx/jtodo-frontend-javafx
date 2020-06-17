db519997ac690b0be636cd2d6910108420d61d46
Felix Pojtinger <felix@pojtinger.com>
2020-06-17 18:32:11 +0200

    refactor: Use relative path to FXML files, include fxml files in jar

--- src/main/java/com/pojtinger/felix/hdm/seOne/jtodo/frontend/Application.java
+++ src/main/java/com/pojtinger/felix/hdm/seOne/jtodo/frontend/Application.java
@@ -1,7 +1,5 @@
 package com.pojtinger.felix.hdm.seOne.jtodo.frontend;
 
-import java.nio.file.Paths;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,12 +7,7 @@ import javafx.stage.Stage;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        var scene = new Scene(FXMLLoader
-                .load(Paths.get("src/main/java/com/pojtinger/felix/hdm/seOne/jtodo/frontend/todos/TodosView.fxml")
-                        .toUri().toURL()));
-        scene.getStylesheets()
-                .add(Paths.get("src/main/java/com/pojtinger/felix/hdm/seOne/jtodo/frontend/ApplicationStyles.css")
-                        .toUri().toURL().toExternalForm());
+        var scene = new Scene(FXMLLoader.load(getClass().getResource("./todos/TodosView.fxml")));
 
         primaryStage.setTitle("JTodo");
         primaryStage.setScene(scene);
