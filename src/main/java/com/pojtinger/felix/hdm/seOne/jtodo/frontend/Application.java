6926372af71cd7e7e5a88fe47c193e2051057f80
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-06-17 18:01:30 +0200

    refactor: Resolve absolute paths for resources

--- src/main/java/com/pojtinger/felix/hdm/seOne/jtodo/frontend/Application.java
+++ src/main/java/com/pojtinger/felix/hdm/seOne/jtodo/frontend/Application.java
@@ -1,5 +1,7 @@
 package com.pojtinger.felicitas.hdm.seOne.jtodo.frontend;
 
+import java.nio.file.Paths;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -7,8 +9,12 @@ import javafx.stage.Stage;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        var scene = new Scene(FXMLLoader.load(getClass().getResource("todos/TodosView.fxml")));
-        scene.getStylesheets().add(getClass().getResource("ApplicationStyles.css").toExternalForm());
+        var scene = new Scene(FXMLLoader
+                .load(Paths.get("src/main/java/com/pojtinger/felicitas/hdm/seOne/jtodo/frontend/todos/TodosView.fxml")
+                        .toUri().toURL()));
+        scene.getStylesheets()
+                .add(Paths.get("src/main/java/com/pojtinger/felicitas/hdm/seOne/jtodo/frontend/ApplicationStyles.css")
+                        .toUri().toURL().toExternalForm());
 
         primaryStage.setTitle("JTodo");
         primaryStage.setScene(scene);
