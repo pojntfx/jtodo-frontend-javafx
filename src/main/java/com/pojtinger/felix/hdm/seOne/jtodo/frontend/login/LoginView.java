3b4a0b03fb3a769b7e1e3a440e17cd3219dc511b
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-06-22 21:34:14 +0200

    feat: Remove FXML and move to fully programmatic approach

---
+++ src/main/java/com/pojtinger/felix/hdm/seOne/jtodo/frontend/login/LoginView.java
@@ -0,0 +1,39 @@
+package com.pojtinger.felicitas.hdm.seOne.jtodo.frontend.login;
+
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class LoginView {
+    private final LoginController controller;
+    private final LoginModel model;
+
+    private BorderPane view;
+
+    public LoginView(LoginController controller, LoginModel model) {
+        this.controller = controller;
+        this.model = model;
+
+        this.initialize();
+    }
+
+    private void initialize() {
+        this.view = new BorderPane();
+
+        var vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+
+        var usernameField = new TextField("Username");
+        usernameField.setMaxWidth(400);
+
+        vbox.getChildren().add(usernameField);
+
+        this.view.setCenter(vbox);
+    }
+
+    public Parent asParent() {
+        return view;
+    }
+}
