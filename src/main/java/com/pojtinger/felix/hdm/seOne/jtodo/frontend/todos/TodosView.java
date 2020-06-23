c00710a44723b45b566753fb6c908dc0d9a272c9
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-06-23 19:47:19 +0200

    feat: Add create button and new todo title text field

--- src/main/java/com/pojtinger/felix/hdm/seOne/jtodo/frontend/todos/TodosView.java
+++ src/main/java/com/pojtinger/felix/hdm/seOne/jtodo/frontend/todos/TodosView.java
@@ -1,17 +1,36 @@
 package com.pojtinger.felicitas.hdm.seOne.jtodo.frontend.todos;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 public class TodosView {
     private TodosModel model;
     private TodosController controller;
 
     public BorderPane root;
+    private TextField newTodoTitleTextField;
+    private Button createTodoButton;
 
     public TodosView(TodosModel model, TodosController controller) {
         this.model = model;
         this.controller = controller;
 
         this.root = new BorderPane();
+        this.root.setPadding(new Insets(8));
+
+        this.newTodoTitleTextField = new TextField();
+        this.newTodoTitleTextField.setPromptText("New todo title");
+        HBox.setHgrow(this.newTodoTitleTextField, Priority.ALWAYS);
+
+        this.createTodoButton = new Button("Create todo");
+
+        var toolbar = new HBox(this.newTodoTitleTextField, this.createTodoButton);
+        toolbar.setSpacing(8);
+
+        this.root.setTop(toolbar);
     }
 }
